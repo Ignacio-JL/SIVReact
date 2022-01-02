fbe124f21a954b447ba2d0160f28efa00d227180
IgnacioPC <joseignacio439@gmail.com>
2022-01-02 01:46:51 -0300

    Desafio 5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import ItemCount from './component/ItemCount';
 import ItemListContainer from './component/ItemListContainer';
 import NavBar from './component/NavBar';
 
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
         <NavBar />
         <ItemListContainer tienda={nombreTienda}/>
+        <ItemCount/>
     </div>
   );
 }
---
+++ src/component/ItemCount.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {useState} from 'react'
+import Button from 'react-bootstrap/Button';
+
+
+
+
+function ItemCount() {
+    const [count, setCount] = useState(1);
+    let stock = 10;
+
+    const btnContador = () =>{
+        if(count<stock){
+            setCount(count+1);
+        }
+        else{
+            alert('Stock Maximo');
+        }
+    }
+    const btnContadorNot = () =>{
+        if(count>=1){
+            setCount(count-1);
+        }
+        else{
+            alert('La cantidad no puede ser menor a 0');
+        }
+    }
+
+    return (
+        <div>
+            <center>
+                <h3>Producto X</h3>
+                <Button variant="primary" onClick={btnContadorNot}>-</Button><span style={{paddingLeft: 10, paddingRight: 10}}>{count}</span><Button variant="primary" onClick={btnContador}>+</Button>
+            </center>
+        </div>
+    )
+}
+
+export default ItemCount
