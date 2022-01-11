e0b17a9c53ba1d0ddf246d3083779bc23451baff
IgnacioPC <joseignacio439@gmail.com>
2022-01-11 18:51:51 -0300

    Entrega Mitad

--- src/component/ItemDetailContainer/ItemDetail/ItemDetail.jsx
+++ src/component/ItemDetailContainer/ItemDetail/ItemDetail.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 function ItemDetail({producto}) {
     return (
         <center>
-            <h3>Detalle Modal</h3>
+            <h3>{producto.name} ({producto.category})</h3>
             <div className='col-md-6'>
-                <li>{producto.name} ({producto.category})</li>
                 <li>{producto.material}</li>
                 <li>Talles {producto.sizeMin} a {producto.sizeMax}</li>
                 <li>Precio c/u $ {producto.price}</li>
